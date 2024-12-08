d2dd2ddca996b68666d71db1877281d964fbe425
Joe Trumbull <trumbull.j@gmail.com>
2024-12-08 14:15:03 -0500

    Fixed encoding issue with images

--- build/images/build.js
+++ build/images/build.js
@@ -1,5 +1,5 @@
 import { src, dest } from 'gulp'
 
 export default function buildImages(cb) {
-  return src('src/images/**/*').pipe(dest('www/images'))
+  return src('src/images/**/*', { encoding: false }).pipe(dest('www/images'))
 }
